a31258f8dbd6567dfb0a0129f4e35015019a3d33
Christian Rojas <cjrojas72@gmail.com>
2020-05-23 22:45:29 -0400

    add ticket and profile view

--- post_bugs/urls.py
+++ post_bugs/urls.py
@@ -3,5 +3,9 @@ from post_bugs import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('addticket/', views.add_ticket_view)
+    path('addticket/', views.add_ticket_view),
+    path('ticket/<int:id>', views.ticketview),
+    path('profile/<int:id>', views.userview),
+    path('login/', views.loginview, name="login"),
+    path('logout/', views.logoutview),
 ]
--- post_bugs/views.py
+++ post_bugs/views.py
@@ -3,10 +3,30 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from post_bugs.models import CustUser, Post
-from post_bugs.forms import AddPostForm
+from post_bugs.forms import AddPostForm, LoginForm
 from bug_tracker.settings import AUTH_USER_MODEL
 
 
+def loginview(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get('next', reverse('home')))
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logoutview(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
+
+
+@login_required
 def index(request):
     new_data = Post.objects.filter(status="NW")
     inProgress_data = Post.objects.filter(status="IP")
@@ -14,6 +34,7 @@ def index(request):
     return render(request, 'index.html', {'new': new_data, 'in_progress': inProgress_data, 'done': done_data})
 
 
+@login_required
 def add_ticket_view(request):
     if request.method == "POST":
         form = AddPostForm(request.POST)
@@ -32,3 +53,14 @@ def add_ticket_view(request):
     form = AddPostForm()
 
     return render(request, 'postticket.html', {"form": form})
+
+
+def ticketview(request, id):
+    data = Post.objects.get(id=id)
+    return render(request, 'ticketview.html', {'data': data})
+
+
+def userview(request, id):
+    author = CustUser.objects.get(id=id)
+    posts = Post.objects.filter(author=author)
+    return render(request, 'userview.html', {'author': author, 'posts': posts})
